90ac9f2a5ff15f64a06069987815c8c6bab76412
Justin Vallelonga <jvallelonga@tangogroup.com>
2015-02-25 12:02:00 -0700

    removes output and allows 80 char width

--- script.sh
+++ script.sh
@@ -1,24 +1,19 @@
 term_width=`tput cols`
-echo $term_width
 file_width=200
 test_width=200
-while [ $test_width -gt 80 ]
+while [ $test_width -gt 60 ]
 do
   if [ $term_width -gt $test_width ]
   then
     file_width=$test_width
-    test_width=80
+    test_width=60
   else
     test_width=$[$test_width-20]
   fi
 done
 
-echo $file_width
-
 url="https://raw.githubusercontent.com/jlvallelonga/not-the-files/master/obiwans/"$file_width".txt"
 
-curl -o ~/.obiwan $url
+curl -s -o ~/.obiwan $url
 
 echo "alias ls='ls | cat ~/.obiwan && echo && echo \"THESE ARE NOT THE FILES YOU ARE LOOKING FOR\" && echo && ls'" >> ~/.bashrc
-
-cat ~/.bashrc
